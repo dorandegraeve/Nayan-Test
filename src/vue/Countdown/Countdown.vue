d56df73fb5caa39a2d64d2daf2de0a8662835c5a
Doran <degraevedoran@gmail.com>
2022-06-06 23:53:01 +0200

    semantics

--- src/vue/Countdown/Countdown.vue
+++ src/vue/Countdown/Countdown.vue
@@ -1,15 +1,14 @@
 <template>
     <section class="countdown">
-        <h1 class="countdown__title">{{ title }}</h1>
-        <h1 class="countdown__subtitle">{{ subtitle }}</h1>
+        <h1 class="countdown__title">{{ title }} <br><span class="countdown__title--breakline">{{ subtitle }}</span></h1>   
         <Counter
             :year="2022"
             :month="11"
-            :day="22"
-            :hour="23"
-            :minute="59"
-            :second="59"
-            :millisecond="59"
+            :day="25"
+            :hour="0"
+            :minute="0"
+            :second="0"
+            :millisecond="0"
         />
     </section>
 </template>
@@ -32,6 +31,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../../scss/main.scss';
+@import '../../scss/abstracts/mixins';
+@import '../../scss/abstracts/variables';
 @import './Countdown.scss';
 </style>
